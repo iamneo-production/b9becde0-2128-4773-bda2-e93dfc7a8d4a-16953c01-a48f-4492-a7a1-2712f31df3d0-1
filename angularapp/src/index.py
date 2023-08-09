66da320b221f87d188e4578f55579ab3458752f5
sunilswizy <sunilsubash006@gmail.com>
2023-08-09 13:31:50 +0000

    ms

---
+++ angularapp/src/index.py
@@ -0,0 +1,298 @@
+print("fdasgasd")
+print("fdasgasd")
+print("fdasgasd")
+print("fdasgasd")
+print("fdasgasd")
+print("fdasgasd")
+print("fdasgasd")
+print("fdasgasd")
+print("fdasgasd")
+print("fdasgasd")
+print("fdasgasd")
+print("fdasgasd")
+print("fdasgasd")
+print("fdasgasd")
+print("fdasgasd")
+print("fdasgasd")
+print("fdasgasd")
+print("fdasgasd")
+print("fdasgasd")
+print("fdasgasd")
+print("fdasgasd")
+print("fdasgasd")
+print("fdasgasd")
+print("fdasgasd")
+print("fdasgasd")
+print("fdasgasd")
+print("fdasgasd")
+print("fdasgasd")
+print("fdasgasd")
+print("fdasgasd")
+print("fdasgasd")
+print("fdasgasd")
+print("fdasgasd")
+print("fdasgasd")
+print("fdasgasd")
+print("fdasgasd")
+print("fdasgasd")
+print("fdasgasd")
+print("fdasgasd")
+print("fdasgasd")
+print("fdasgasd")
+print("fdasgasd")
+print("fdasgasd")
+print("fdasgasd")
+print("fdasgasd")
+print("fdasgasd")
+print("fdasgasd")
+print("fdasgasd")
+print("fdasgasd")
+print("fdasgasd")
+print("fdasgasd")
+print("fdasgasd")
+print("fdasgasd")
+print("fdasgasd")
+print("fdasgasd")
+print("fdasgasd")
+print("fdasgasd")
+print("fdasgasd")
+print("fdasgasd")
+print("fdasgasd")
+print("fdasgasd")
+print("fdasgasd")
+print("fdasgasd")
+print("fdasgasd")
+print("fdasgasd")
+print("fdasgasd")
+print("fdasgasd")
+print("fdasgasd")
+print("fdasgasd")
+print("fdasgasd")
+print("fdasgasd")
+print("fdasgasd")
+print("fdasgasd")
+print("fdasgasd")
+print("fdasgasd")
+print("fdasgasd")
+print("fdasgasd")
+print("fdasgasd")
+print("fdasgasd")
+print("fdasgasd")
+print("fdasgasd")
+print("fdasgasd")
+print("fdasgasd")
+print("fdasgasd")
+print("fdasgasd")
+print("fdasgasd")
+print("fdasgasd")
+print("fdasgasd")
+
+
+
+
+
+for i in range(1000):
+    print("SFDAfsda")
+
+
+
+for i in range(1000):
+    print("SFDAfsda")
+for i in range(1000):
+    print("SFDAfsda")
+for i in range(1000):
+    print("SFDAfsda")
+for i in range(1000):
+    print("SFDAfsda")
+for i in range(1000):
+    print("SFDAfsda")
+for i in range(1000):
+    print("SFDAfsda")
+for i in range(1000):
+    print("SFDAfsda")
+for i in range(1000):
+    print("SFDAfsda")
+for i in range(1000):
+    print("SFDAfsda")
+for i in range(1000):
+    print("SFDAfsda")
+for i in range(1000):
+    print("SFDAfsda")
+for i in range(1000):
+    print("SFDAfsda")
+for i in range(1000):
+    print("SFDAfsda")
+for i in range(1000):
+    print("SFDAfsda")
+for i in range(1000):
+    print("SFDAfsda")
+for i in range(1000):
+    print("SFDAfsda")
+for i in range(1000):
+    print("SFDAfsda")
+for i in range(1000):
+    print("SFDAfsda")
+for i in range(1000):
+    print("SFDAfsda")
+for i in range(1000):
+    print("SFDAfsda")
+for i in range(1000):
+    print("SFDAfsda")
+for i in range(1000):
+    print("SFDAfsda")
+for i in range(1000):
+    print("SFDAfsda")
+for i in range(1000):
+    print("SFDAfsda")
+for i in range(1000):
+    print("SFDAfsda")
+for i in range(1000):
+    print("SFDAfsda")
+for i in range(1000):
+    print("SFDAfsda")
+for i in range(1000):
+    print("SFDAfsda")
+for i in range(1000):
+    print("SFDAfsda")
+for i in range(1000):
+    print("SFDAfsda")
+for i in range(1000):
+    print("SFDAfsda")
+for i in range(1000):
+    print("SFDAfsda")
+for i in range(1000):
+    print("SFDAfsda")
+for i in range(1000):
+    print("SFDAfsda")
+for i in range(1000):
+    print("SFDAfsda")
+for i in range(1000):
+    print("SFDAfsda")
+for i in range(1000):
+    print("SFDAfsda")
+for i in range(1000):
+    print("SFDAfsda")
+for i in range(1000):
+    print("SFDAfsda")
+for i in range(1000):
+    print("SFDAfsda")
+for i in range(1000):
+    print("SFDAfsda")
+for i in range(1000):
+    print("SFDAfsda")
+for i in range(1000):
+    print("SFDAfsda")
+for i in range(1000):
+    print("SFDAfsda")
+for i in range(1000):
+    print("SFDAfsda")
+for i in range(1000):
+    print("SFDAfsda")
+for i in range(1000):
+    print("SFDAfsda")
+for i in range(1000):
+    print("SFDAfsda")
+for i in range(1000):
+    print("SFDAfsda")
+for i in range(1000):
+    print("SFDAfsda")
+for i in range(1000):
+    print("SFDAfsda")
+for i in range(1000):
+    print("SFDAfsda")
+for i in range(1000):
+    print("SFDAfsda")
+for i in range(1000):
+    print("SFDAfsda")
+for i in range(1000):
+    print("SFDAfsda")
+for i in range(1000):
+    print("SFDAfsda")
+for i in range(1000):
+    print("SFDAfsda")
+for i in range(1000):
+    print("SFDAfsda")
+for i in range(1000):
+    print("SFDAfsda")
+for i in range(1000):
+    print("SFDAfsda")
+for i in range(1000):
+    print("SFDAfsda")
+for i in range(1000):
+    print("SFDAfsda")
+for i in range(1000):
+    print("SFDAfsda")
+for i in range(1000):
+    print("SFDAfsda")
+for i in range(1000):
+    print("SFDAfsda")
+for i in range(1000):
+    print("SFDAfsda")
+for i in range(1000):
+    print("SFDAfsda")
+for i in range(1000):
+    print("SFDAfsda")
+for i in range(1000):
+    print("SFDAfsda")
+for i in range(1000):
+    print("SFDAfsda")
+for i in range(1000):
+    print("SFDAfsda")
+for i in range(1000):
+    print("SFDAfsda")
+for i in range(1000):
+    print("SFDAfsda")
+for i in range(1000):
+    print("SFDAfsda")
+for i in range(1000):
+    print("SFDAfsda")
+for i in range(1000):
+    print("SFDAfsda")
+for i in range(1000):
+    print("SFDAfsda")
+for i in range(1000):
+    print("SFDAfsda")
+for i in range(1000):
+    print("SFDAfsda")
+for i in range(1000):
+    print("SFDAfsda")
+for i in range(1000):
+    print("SFDAfsda")
+for i in range(1000):
+    print("SFDAfsda")
+for i in range(1000):
+    print("SFDAfsda")
+for i in range(1000):
+    print("SFDAfsda")
+for i in range(1000):
+    print("SFDAfsda")
+for i in range(1000):
+    print("SFDAfsda")
+for i in range(1000):
+    print("SFDAfsda")
+for i in range(1000):
+    print("SFDAfsda")
+for i in range(1000):
+    print("SFDAfsda")
+for i in range(1000):
+    print("SFDAfsda")
+for i in range(1000):
+    print("SFDAfsda")
+for i in range(1000):
+    print("SFDAfsda")
+for i in range(1000):
+    print("SFDAfsda")
+for i in range(1000):
+    print("SFDAfsda")
+for i in range(1000):
+    print("SFDAfsda")
+for i in range(1000):
+    print("SFDAfsda")
+for i in range(1000):
+    print("SFDAfsda")
+for i in range(1000):
+    print("SFDAfsda")
+for i in range(1000):
+    print("SFDAfsda")
+for i in range(1000):
+    print("SFDAfsda")
